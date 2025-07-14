9b025d9ac2be35c7435063cbea259eb9896ef1a5
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-07-14 11:38:03 -0300

    build: config dev creating migrations and making build project

---
+++ priv/repo/migrations/20250714142136_create_payments.exs
@@ -0,0 +1,16 @@
+defmodule RinhaBackend.Repo.Migrations.CreatePayments do
+  use Ecto.Migration
+
+  def change do
+    create table(:payments, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :correlation_id, :uuid, null: false
+      add :amount, :decimal, null: false
+      add :processor, :string, null: false
+      add :requested_at, :utc_datetime_usec, null: false
+
+      timestamps()
+    end
+    create unique_index(:payments, [:correlation_id])
+  end
+end
